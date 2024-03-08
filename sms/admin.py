bdc2ecd55da06d8115d2d6552b38ead6db07dde2
mohammad goodarzi <m.goodarzi606@gmail.com>
2024-03-09 00:40:28 +0330

    add model signle and multiple send message

--- sms/admin.py
+++ sms/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-from sms.models import UploadFileModel, SendSmsModel
+from sms.models import UploadFileModel, SendSingleMessageModel,  SendMultiplesMessageModel, PhoneBookModel
 
 
 @admin.register(UploadFileModel)
 class UploadFileModelAdmin(admin.ModelAdmin):
     list_display = ('execl_file', 'created_at', 'updated_at')
+    list_display_links = ('created_at', 'updated_at')
 
 
-@admin.register(SendSmsModel)
+@admin.register(SendSingleMessageModel)
 class SendSmsModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SendMultiplesMessageModel)
+class SendSmsModelAdmin(admin.ModelAdmin):
+    filter_horizontal = ('m2m_mobile_phone',)
+
+
+@admin.register(PhoneBookModel)
+class PhoneBookModelAdmin(admin.ModelAdmin):
     pass
--- sms/models.py
+++ sms/models.py
@@ -13,6 +13,11 @@ class UploadFileModel(CreateModel, UpdateModel):
         upload_to='execl_files/%y/%m/%d',
         validators=[FileExtensionValidator(allowed_extensions=['xls', 'xlsx'])]
     )
+    alt = models.TextField(_('توضیحی در مورد فایل'), blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.alt} -- {self.id}'
 
     @property
     def mobile_phone(self):
@@ -28,19 +33,81 @@ class UploadFileModel(CreateModel, UpdateModel):
         birth_day = pandas_excel_file['birthday']
         return birth_day
 
+    @property
+    def contract(self):
+        read_excel_file = self.execl_file
+        pandas_excel_file = pd.read_excel(read_excel_file)
+        contract = pandas_excel_file['contract']
+        return contract
 
-class SendSmsModel(CreateModel):
-    execl_file = models.FileField(upload_to='execl_files/%y/%m/%d')
+    @property
+    def third_party_insurance(self):
+        read_excel_file = self.execl_file
+        pandas_excel_file = pd.read_excel(read_excel_file)
+        third_party_insurance = pandas_excel_file['Third party insurance']
+        return third_party_insurance
+
+    @property
+    def rental_insurance(self):
+        read_excel_file = self.execl_file
+        pandas_excel_file = pd.read_excel(read_excel_file)
+        rental_insurance = pandas_excel_file['rental insurance']
+        return rental_insurance
+
+    @property
+    def technical_diagnosis(self):
+        read_excel_file = self.execl_file
+        pandas_excel_file = pd.read_excel(read_excel_file)
+        technical_diagnosis = pandas_excel_file['technical diagnosis']
+        return technical_diagnosis
+
+
+class PhoneBookModel(CreateModel, UpdateModel):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    mobile_phone = models.CharField(max_length=15, unique=True)
+    is_active = models.BooleanField(default=True)
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return f'{self.full_name} {self.mobile_phone}'
+
+    class Meta:
+        db_table = 'phone_book'
+        verbose_name = _("Phone Book")
+        verbose_name_plural = _("Phone Books")
+
+
+class SendSingleMessageModel(CreateModel):
     from_user = models.ForeignKey(verbose_name='از کاربر', to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT,
-                                  related_name='sender', limit_choices_to={'is_superuser': True})
-    to_mobile_phone = models.ForeignKey(verbose_name=_("به شماره همراه"), to=UploadFileModel,
-                                        related_name='upload_file',
-                                        blank=True, null=True, on_delete=models.PROTECT)
+                                  related_name='sender_single_sms', limit_choices_to={'is_superuser': True})
+    to_mobile_phone = models.CharField(max_length=15, unique=True)
+    foreignkey_mobile_phone = models.ForeignKey(PhoneBookModel, on_delete=models.PROTECT,
+                                                related_name='foreignkey_mobile_phone', blank=True, null=True)
+    title_message = models.CharField(_("عنوان پیام"), blank=True, null=True, max_length=120)
     message_body = models.TextField(_('متن پیام'))
     send_time = django_jalali_models.jDateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         db_table = 'send_sms'
-        verbose_name = _('send sms')
-        verbose_name_plural = _("send_sms")
+        verbose_name = _('send single sms')
+        verbose_name_plural = _("send single sms")
 
+
+class SendMultiplesMessageModel(CreateModel, UpdateModel):
+    from_user = models.ForeignKey(verbose_name='از کاربر', to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT,
+                                  related_name='sender_multiple_sms', limit_choices_to={'is_superuser': True})
+    m2m_mobile_phone = models.ManyToManyField(PhoneBookModel, related_name='m2m_mobile_phone',)
+    title_message = models.CharField(_("عنوان پیام"), blank=True, null=True, max_length=120)
+    message_body = models.TextField(_('متن پیام'))
+    send_time = django_jalali_models.jDateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'send_multiple_messages'
+        verbose_name = _('send multiple sms')
+        verbose_name_plural = _("send multiple sms")
