dbfa98e3e95b45563e074d66d7a69a65c715b1ff
mohammad goodarzi <m.goodarzi606@gmail.com>
2024-04-06 19:29:39 +0330

    add config beat_schedule

--- send_sms/celery.py
+++ send_sms/celery.py
@@ -1,9 +1,35 @@
 from celery import Celery
 import os
 
+from celery.schedules import crontab
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'send_sms.settings')
 
 app = Celery('send_sms')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+
+app.conf.beat_schedule = {
+    'task_send_sms_birthday': {
+        'task': 'sms.task_celery.task_send_sms_birthday',
+        'schedule': crontab(hour='8', minute='0'),
+    },
+    'task_send_sms_contact': {
+        'task': 'sms.task_celery.task_send_sms_contract',
+        'schedule': crontab(hour='8', minute='0')
+    },
+    'task_send_sms_third_rental_insurance': {
+        'task': 'sms.task_celery.task_send_sms_third_party_insurance',
+        'schedule': crontab(hour='8', minute='0')
+    },
+    'task_send_sms_rental_insurance': {
+        'task': 'sms.task_celery.task_send_sms_rental_insurance',
+        'schedule': crontab(hour='8', minute='0')
+    },
+    'task_send_sms_technical_diagnoses': {
+        'task': 'sms.task_celery.task_send_sms_technical_diagnoses',
+        'schedule': crontab(hour='8', minute='0')
+    },
+
+}
